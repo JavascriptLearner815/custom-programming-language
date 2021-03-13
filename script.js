16f437ca6274fcaf4a6d180cc5bf4aa069755e63
Dean Alan Summer <deanlovesmargie@gmail.com>
2021-03-13 14:26:58 -0600

    Comment RegExp

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
   "But I don't work!"
   console Invalid operations followed by valid operations will execute the valid operations!
   Invalid operations like this will be ignored and won't throw an error.
-  console Use this hack to add comments! Invalid arguments like this will be ignored and won't throw an error, as well.
+  : Use this to add comments! :
   globalvar "hellothere" is "Some text"
   globalvar "nothing" noval
   getvarandconsole "hellothere"
@@ -26,6 +26,7 @@
   const globalVarRegExp = /(?:^|\s+)globalvar:?\s+\\?"(.+?)\\?",?\s?(?:is:?|:|=)\s?\\?"(.+?)\\?"+;?/gim;
   const globalVarNoValRegExp = /(?:^|\s+)globalvar:?\s+\\?"(.+?)\\?",?\s+noval+;?/gim;
   const getVarAndConsoleRegExp = /(?:^|\s+)getvarandconsole:?\s+\\?"(.+?)\\?";?/gim;
+  const commentRegExp = /(?:^|\s+):\s?([^:]*);?/gim;
   
   btn.addEventListener("click", () => {
     const { value } = code;
@@ -34,6 +35,9 @@
     [...value.matchAll(globalVarRegExp)].forEach(m => globalVariables[m[1]] = m[2]);
     [...value.matchAll(globalVarNoValRegExp)].forEach(m => globalVariables[m[1]] = null);
     [...value.matchAll(getVarAndConsoleRegExp)].forEach(m => console.log(globalVariables[m[1]] ?? "Unspecified"));
+    [...value.matchAll(commentRegExp)].forEach(m => {
+      ;
+    });
     
     setTimeout(() => {
       alert("Code executed; remember to check your console (Ctrl+Shift+I or Command+Shift+I)!");
